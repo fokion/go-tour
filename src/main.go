5f56f28ac349607e3f1a4586d2867436cc608a8e
Fokion <fokion@textensor.com>
2017-01-16 21:22:57 +0000

    for loop

--- src/main.go
+++ src/main.go
@@ -1,12 +1,20 @@
 package main
-import(
-  "fmt"
+
+import (
+	"fmt"
 )
-func swap(x int ,y  string) (string,int){
-  return y, x
+
+func swap(x int, y string) (string, int) {
+	return y, x
 }
-func main()  {
-a,b := swap(3,"test");
-fmt.Println(a,b)  
-fmt.Printf("Test %T type and %v value\n",a,a)
+func main() {
+	a, b := swap(3, "test")
+	fmt.Println(a, b)
+	fmt.Printf("Test %T type and %v value\n", a, a)
+	sum := 0
+	count := 50
+	for i := 0; i < count; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
